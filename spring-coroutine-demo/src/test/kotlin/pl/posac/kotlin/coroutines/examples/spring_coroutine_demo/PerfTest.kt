f65f15ff5b70529004bcc8498edb034edd974a76
michal.posacki <michal.posacki@kuehne-nagel.com>
2022-05-27 10:18:14 +0200

    remove ktor and use webclient

--- spring-coroutine-demo/src/test/kotlin/pl/posac/kotlin/coroutines/examples/spring_coroutine_demo/PerfTest.kt
+++ spring-coroutine-demo/src/test/kotlin/pl/posac/kotlin/coroutines/examples/spring_coroutine_demo/PerfTest.kt
@@ -1,23 +1,49 @@
 package pl.posac.kotlin.coroutines.examples.spring_coroutine_demo
 
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.request.*
+
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import org.springframework.web.reactive.function.client.awaitExchange
+import reactor.netty.http.client.HttpClient
+import reactor.netty.resources.ConnectionProvider
+import java.time.Duration
 
 suspend fun main() = coroutineScope{
-    val coroutinCount = 400
-    val requestCount = 10
-    val client = HttpClient(CIO)
+    val connectionProvider = ConnectionProvider.builder("myConnectionPool")
+        .maxConnections(500)
+        .pendingAcquireMaxCount(5000)
+        .disposeTimeout(Duration.ofMinutes(1))
+        .build()
+    val clientHttpConnector = ReactorClientHttpConnector(HttpClient.create(connectionProvider))
+    for (i in 1..10) {
+            launch {
+                val webClient = WebClient.builder()
+                    .clientConnector(clientHttpConnector)
+                    .build()
 
-    coroutinCount.downTo(0).forEach { coroutinCount ->
-        launch {
-            requestCount.downTo(0).forEach { request ->
-                val response = client.get("http://localhost:8080/getMe/${coroutinCount * requestCount + request}")
+                    .get()
+                coroutineScope{
+                    val coroutinCount = 300
+                    val requestCount = 10
 
+
+                    coroutinCount.downTo(0).forEach { coroutinCount ->
+                        launch {
+                            requestCount.downTo(0).forEach { request ->
+                                val url = "http://localhost:8080/getMe/${coroutinCount * requestCount + request}"
+                                webClient
+                                    .uri(url)
+                                    .awaitExchange {
+                                        it.awaitBody(String::class)
+                                    }
+                            }
+                        }
+                    }
+                }
             }
-        }
     }
+
 }
